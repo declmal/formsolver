8289ff2cf7be1740895f2c81955abb6190772070
ryt <tang_1995@126.com>
2020-11-07 20:16:36 +0800

    update

---
+++ python/element.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+
+class Node:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def get_coord(self):
+        return self.x, self.y, self.z
+    def serialize(self):
+        return ','.join([str(self.x), str((self.y)), str(self.z)])
+
+
+def yaw(theta):
+    cs = np.cos(theta)
+    si = np.sin(theta)
+    return np.array([
+        [cs,-si,0],
+        [si,cs,0],
+        [0,0,1],
+    ])
+
+def pitch(theta):
+    cs = np.cos(theta)
+    si = np.sin(theta)
+    return np.array([
+        [cs,0,si],
+        [0,1,0],
+        [-si,0,cs],
+    ])
+
+def roll(theta):
+    cs = np.cos(theta)
+    si = np.sin(theta)
+    return np.array([
+        [1,0,0],
+        [0,cs,-si],
+        [0,si,cs],
+    ])
+
+def rotate_3d_vec(vec, yawMat, pitchMat, rollMat):
+    return np.matmul(
+        yawMat, np.matmul(
+            pitchMat, np.matmul(rollMat, vec)
+        )
+    )
+
+def rand_num(th=0.1):
+    return np.random.random()*2*th - th
+
+
+class Element:
+    def __init__(self, nodes):
+        self.nodes = nodes
+    def get_nodes(self):
+        return self.nodes
+    def serialize(self, otype="default"):
+        if otype == "default":
+            return '\n'.join(
+                [node.serialize() for node in self.nodes]
+            )
+        elif otype == "form":
+            return '\n'.join(
+                [node.serialize()+',' for node in self.nodes]
+            )
+        elif otype == "inp":
+            return '\n'.join(
+                [
+                    str(i+1)+','+node.serialize()
+                    for i, node in enumerate(self.nodes)
+                ]
+            )
+        else:
+            raise NotImplementedError
+    @staticmethod
+    def rotate_3d(elem, origin=Node(0,0,0), alpha=0.0, beta=0.0, gamma=0.0):
+        nnodes = []
+        xo, yo, zo = origin.get_coord()
+        yawMat = yaw(alpha)
+        pitchMat = pitch(beta)
+        rollMat = roll(gamma)
+        for node in elem.get_nodes():
+            x, y, z = node.get_coord()
+            vec = np.array([x-xo,y-yo,z-zo])
+            nvec = rotate_3d_vec(vec, yawMat, pitchMat, rollMat)
+            ndx, ndy, ndz = nvec.tolist()
+            nx, ny, nz = xo+ndx, yo+ndy, zo+ndz
+            nnodes.append(Node(nx,ny,nz))
+        return type(elem)(nnodes)
+    @staticmethod
+    def perturb(elem, th=0.1):
+        nnodes = []
+        for node in elem.get_nodes():
+            x, y, z = node.get_coord()
+            dx = rand_num(th=th)
+            dy = rand_num(th=th)
+            dz = rand_num(th=th)
+            nx, ny, nz = x+dx, y+dy, z+dz
+            nnodes.append(Node(nx,ny,nz))
+        return type(elem)(nnodes)
+
+
+# C3D20
+c3d20rUnitNodes = [
+    Node(x,y,z) for x, y, z in [
+        [0, 0, 0],
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 1, 1],
+        [0.5, 0, 0],
+        [1, 0.5, 0],
+        [0.5, 1, 0],
+        [0, 0.5, 0],
+        [0.5, 0, 1],
+        [1, 0.5, 1],
+        [0.5, 1, 1],
+        [0, 0.5, 1],
+        [0, 0, 0.5],
+        [1, 0, 0.5],
+        [1, 1, 0.5],
+        [0, 1, 0.5],
+    ]
+]
+c3d20Unit = Element(c3d20rUnitNodes)
+
+# C3D8
+c3d8UnitNodes = [
+    Node(x,y,z) for x,y,z in [
+      [0, 0, 0],
+      [1, 0, 0],
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+      [1, 0, 1],
+      [1, 1, 1],
+      [0, 1, 1],
+    ]
+]
+c3d8Unit = Element(c3d8UnitNodes)
+
+if __name__ == '__main__':
+    th = 0.2
+    elem = Element.perturb(c3d8Unit, th=th)
+    origin = Node(0.5,0.5,0.5)
+    alpha = 0.3
+    beta = 1.5
+    gamma = -2.1
+    elem = Element.rotate_3d(
+        elem, origin=origin,
+        alpha=alpha, beta=beta, gamma=gamma)
+    otype = "inp"
+    print(elem.serialize(otype=otype))
+
--- python/grid.py
+++ python/grid.py
@@ -1,158 +1,235 @@
+import os
+import math
 import numpy as np
+import scipy
+from scipy.spatial import KDTree
+from os import path
 
 
-class Node:
+class Coord:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+
     def get_coord(self):
         return self.x, self.y, self.z
-    def serialize(self):
-        return ','.join([str(self.x), str((self.y)), str(self.z)])
+
+    @staticmethod
+    def get_mid_coord(coord1, coord2):
+        x1, y1, z1 = coord1.get_coord()
+        x2, y2, z2 = coord2.get_coord()
+        return Coord(0.5*(x1+x2), 0.5*(y1+y2), 0.5*(z1+z2))
 
 
-def yaw(theta):
-    cs = np.cos(theta)
-    si = np.sin(theta)
-    return np.array([
-        [cs,-si,0],
-        [si,cs,0],
-        [0,0,1],
-    ])
+class Node:
+    def __init__(self, coord, nodeId):
+        self.coord = coord
+        self.nodeId = nodeId
 
-def pitch(theta):
-    cs = np.cos(theta)
-    si = np.sin(theta)
-    return np.array([
-        [cs,0,si],
-        [0,1,0],
-        [-si,0,cs],
-    ])
+    def get_coord(self):
+        return self.coord.get_coord()
 
-def roll(theta):
-    cs = np.cos(theta)
-    si = np.sin(theta)
-    return np.array([
-        [1,0,0],
-        [0,cs,-si],
-        [0,si,cs],
-    ])
+    def get_id(self):
+        return self.nodeId
 
-def rotate_3d_vec(vec, yawMat, pitchMat, rollMat):
-    return np.matmul(
-        yawMat, np.matmul(
-            pitchMat, np.matmul(rollMat, vec)
-        )
-    )
+    def to_inp(self):
+        x, y, z = self.get_coord()
+        return ','.join([str(self.nodeId), str(x), str(y), str(z)])
 
-def rand_num(th=0.1):
-    return np.random.random()*2*th - th
+
+class NodeSet:
+    def __init__(self, dstTol=1e-6):
+        self.nodeLst = []
+        self.dstTol = dstTol
+
+    def insert_node(self, coord):
+        cx, cy, cz = coord.get_coord()
+        minDst, nodeId = -1, -1
+        for node in self.nodeLst:
+            nx, ny, nz = node.get_coord()
+            dx, dy, dz = nx-cx, ny-cy, nz-cz
+            dst = math.sqrt(dx*dx + dy*dy + dz*dz)
+            if minDst == -1 or dst < minDst:
+                minDst, nodeId = dst, node.get_id()
+        if minDst > self.dstTol or minDst == -1:
+            nodeId = len(self.nodeLst)
+            node = Node(coord, nodeId)
+            self.nodeLst.append(node)
+        return nodeId
+
+    def __iter__(self):
+        return iter(self.nodeLst)
 
 
 class Element:
-    def __init__(self, nodes):
-        self.nodes = nodes
-    def get_nodes(self):
-        return self.nodes
-    def serialize(self, otype="default"):
-        if otype == "default":
-            return '\n'.join(
-                [node.serialize() for node in self.nodes]
-            )
-        elif otype == "form":
-            return '\n'.join(
-                [node.serialize()+',' for node in self.nodes]
-            )
-        elif otype == "inp":
-            return '\n'.join(
-                [
-                    str(i+1)+','+node.serialize()
-                    for i, node in enumerate(self.nodes)
-                ]
-            )
-        else:
-            raise NotImplementedError
+    def __init__(self, elemId, nodeIds):
+        self.elemId = elemId
+        self.nodeIds = nodeIds
+
+    def to_inp(self):
+        return ','.join([
+            str(self.elemId)] +
+            [str(nodeId) for nodeId in self.nodeIds]
+        )
+
     @staticmethod
-    def rotate_3d(elem, origin=Node(0,0,0), alpha=0.0, beta=0.0, gamma=0.0):
-        nnodes = []
-        xo, yo, zo = origin.get_coord()
-        yawMat = yaw(alpha)
-        pitchMat = pitch(beta)
-        rollMat = roll(gamma)
-        for node in elem.get_nodes():
-            x, y, z = node.get_coord()
-            vec = np.array([x-xo,y-yo,z-zo])
-            nvec = rotate_3d_vec(vec, yawMat, pitchMat, rollMat)
-            ndx, ndy, ndz = nvec.tolist()
-            nx, ny, nz = xo+ndx, yo+ndy, zo+ndz
-            nnodes.append(Node(nx,ny,nz))
-        return type(elem)(nnodes)
+    def get_type():
+        raise NotImplementedError
+
     @staticmethod
-    def perturb(elem, th=0.1):
-        nnodes = []
-        for node in elem.get_nodes():
-            x, y, z = node.get_coord()
-            dx = rand_num(th=th)
-            dy = rand_num(th=th)
-            dz = rand_num(th=th)
-            nx, ny, nz = x+dx, y+dy, z+dz
-            nnodes.append(Node(nx,ny,nz))
-        return type(elem)(nnodes)
+    def to_elem(nodeSet, coords, elemId):
+        raise NotImplementedError
 
 
-# C3D20
-c3d20rUnitNodes = [
-    Node(x,y,z) for x, y, z in [
-        [0, 0, 0],
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [1, 0, 1],
-        [1, 1, 1],
-        [0, 1, 1],
-        [0.5, 0, 0],
-        [1, 0.5, 0],
-        [0.5, 1, 0],
-        [0, 0.5, 0],
-        [0.5, 0, 1],
-        [1, 0.5, 1],
-        [0.5, 1, 1],
-        [0, 0.5, 1],
-        [0, 0, 0.5],
-        [1, 0, 0.5],
-        [1, 1, 0.5],
-        [0, 1, 0.5],
-    ]
-]
-c3d20Unit = Element(c3d20rUnitNodes)
+class C3D8(Element):
+    def __init__(self, elemId, nodeIds):
+        assert len(nodeIds) == 8
+        super().__init__(elemId, nodeIds)
 
-# C3D8
-c3d8UnitNodes = [
-    Node(x,y,z) for x,y,z in [
-      [0, 0, 0],
-      [1, 0, 0],
-      [1, 1, 0],
-      [0, 1, 0],
-      [0, 0, 1],
-      [1, 0, 1],
-      [1, 1, 1],
-      [0, 1, 1],
-    ]
-]
-c3d8Unit = Element(c3d8UnitNodes)
+    @staticmethod
+    def get_type():
+        return "C3D8"
 
-if __name__ == '__main__':
-    th = 0.2
-    elem = Element.perturb(c3d8Unit, th=th)
-    origin = Node(0.5,0.5,0.5)
-    alpha = 0.3
-    beta = 1.5
-    gamma = -2.1
-    elem = Element.rotate_3d(
-        elem, origin=origin,
-        alpha=alpha, beta=beta, gamma=gamma)
-    otype = "inp"
-    print(elem.serialize(otype=otype))
+    @staticmethod
+    def to_elem(nodeSet, coords, elemId):
+        return C3D8(elemId, [
+            nodeSet.insert_node(coord) for coord in coords
+        ])
 
+class C3D8I(C3D8):
+    @staticmethod
+    def get_type():
+        return "C3D8I"
+
+
+class C3D20(Element):
+    def __init__(self, elemId, nodeIds):
+        assert len(nodeIds) == 20
+        super().__init__(elemId, nodeIds)
+
+    @staticmethod
+    def get_type():
+        return "C3D20"
+
+    @staticmethod
+    def to_elem(nodeSet, coords, elemId):
+        ncoords = [
+            Coord.get_mid_coord(coords[0],coords[1]),
+            Coord.get_mid_coord(coords[1],coords[2]),
+            Coord.get_mid_coord(coords[2],coords[3]),
+            Coord.get_mid_coord(coords[3],coords[1]),
+            Coord.get_mid_coord(coords[4],coords[5]),
+            Coord.get_mid_coord(coords[5],coords[6]),
+            Coord.get_mid_coord(coords[6],coords[7]),
+            Coord.get_mid_coord(coords[7],coords[4]),
+            Coord.get_mid_coord(coords[0],coords[4]),
+            Coord.get_mid_coord(coords[1],coords[5]),
+            Coord.get_mid_coord(coords[2],coords[6]),
+            Coord.get_mid_coord(coords[3],coords[7]),
+        ]
+        return C3D20(elemId, [
+            nodeSet.insert_node(coord) for coord in coords+ncoords
+        ])
+
+
+class C3D20R(C3D20):
+    @staticmethod
+    def get_type():
+        return "C3D20R"
+
+
+class ElemSet:
+    def __init__(self):
+        self.elemLst = []
+
+    def create_elem(self, elemType, nodeSet, coords):
+        elemId = len(self.elemLst)
+        elem = elemType.to_elem(nodeSet, coords, elemId)
+        self.elemLst.append(elem)
+
+    def __iter__(self):
+        return iter(self.elemLst)
+
+
+class GeoType:
+    def create_mesh(self):
+        raise NotImplementedError
+
+
+class TunnelGeoType(GeoType):
+    def __init__(
+        self, depth=100000, num_layers=500, r_out=9000, r_in=8500,
+        num_loops=3, num_slices=50, orgCoord=Coord(0,0,0)):
+        print("num element:", num_layers*num_loops*num_slices)
+        self.depth = depth
+        self.num_layers = num_layers
+        self.r_out = r_out
+        self.r_in = r_in
+        self.num_loops = num_loops
+        self.num_slices = num_slices
+        self.orgCoord = orgCoord
+
+    def create_mesh(self, elemType):
+        dy = self.depth / self.num_layers
+        dr = (self.r_out-self.r_in) / self.num_loops
+        dphi = 360 / self.num_slices
+        ox, oy, oz = self.orgCoord.get_coord()
+        cy = oy
+        elemSet = ElemSet()
+        nodeSet = NodeSet()
+        for i in range(self.num_layers):
+            ny = cy + dy
+            cr = self.r_in
+            for j in range(self.num_loops):
+                nr = cr + dr
+                cphi = 0
+                for k in range(self.num_slices):
+                    nphi = cphi + dphi
+                    coords = [
+                        self._get_coord(cy, cr, cphi),
+                        self._get_coord(cy, cr, nphi),
+                        self._get_coord(ny, cr, nphi),
+                        self._get_coord(ny, cr, cphi),
+                        self._get_coord(cy, nr, cphi),
+                        self._get_coord(cy, nr, nphi),
+                        self._get_coord(ny, nr, nphi),
+                        self._get_coord(ny, nr, cphi),
+                    ]
+                    elemSet.create_elem(elemType, nodeSet, coords)
+                    cphi = nphi
+                cr = nr
+            cy = ny
+        return elemSet, nodeSet
+
+    def _get_coord(self, y, r, phi):
+        x = r * np.sin(phi)
+        z = r * np.cos(phi)
+        return Coord(x, y, z)
+
+
+class Model:
+    def __init__(self, geo, elemType):
+        self.elemSet, self.nodeSet = geo.create_mesh(elemType)
+
+    def to_inp(self, modelName="model", modelDir=path.expanduser("~/.model")):
+        inpContent = "*Node\n"
+        inpContent += '\n'.join(
+            [node.to_inp() for node in self.nodeSet]
+        )
+        inpContent = "*Element\n"
+        inpContent += '\n'.join(
+            [elem.to_inp() for elem in self.elemSet]
+        )
+        modelPath = path.join(modelDir, modelName+'.inp')
+        with open(modelPath, 'w') as f:
+            f.write(inpContent)
+
+    def to_json(self, modelName="model", modelDir=path.expanduser("~/.model")):
+        pass
+
+if __name__ == "__main__":
+    geo = TunnelGeoType()
+    elemType = C3D20
+    model = Model(geo, elemType)
+    model.to_inp()
